599f5f16d5c8bfe495857326ce9ec8ab039493d3
kongqi <13951113338@163.com>
2019-09-17 18:04:13 +0800

    更新jenkins触发

---
+++ src/main/java/com/example/quartz/controller/JenkinsController.java
@@ -0,0 +1,20 @@
+package com.example.quartz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: Mr.Kong
+ * @Date: 2019/9/17 17:09
+ */
+@RequestMapping(value = "/jenkins")
+@RestController
+public class JenkinsController {
+
+    @GetMapping("/trigger")
+    public Boolean trigger(){
+        return true;
+    }
+}
