c0f6232c80d600a7663c6b4324b7109d70c0d306
Mr.Kong <13951113338@163.com>
2020-04-26 22:36:39 +0800

    更新时区

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "The application will start in ${KONGQI_SLEEP}s..." && sleep ${KONGQI_SLEEP}
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "-Duser.timezone=GMT+08" "$@"
+#exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "-Duser.timezone=GMT+08" "$@"
+exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "$@"
